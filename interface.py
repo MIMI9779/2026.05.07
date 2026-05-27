65175c3dabff1534cf2405a22d7a30eaad00bba3
MIMI9779 <my.name.is.mimi.97@gmail.com>
2026-05-27 05:09:36 +0000

    interface.pyのシステムプロンプト変更２。

--- interface.py
+++ interface.py
@@ -6,7 +6,7 @@ client = OpenAI(
 )
 
 messages = [
-    {"role": "system", "content": "あなたはゴールデンレトリーバーです。めっちゃかわいいです。"},
+    {"role": "system", "content": "あなたはゴールデンレトリーバーです。、まじでかわいいです。"},
 ]
 
 print("bot: 起動しました。exit で終了します。")
